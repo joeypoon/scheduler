caa1a91b7ee27f552af1a843b3c5e25e4c4c6b5c
Joey <joey@joeypoon.com>
2015-05-11 16:29:57 -0500

    Add employee and schedule classes

--- scheduler.rb
+++ scheduler.rb
@@ -10,19 +10,48 @@
 
 #representation *****
 #8am = 0
-#9am = 60
+#900 = 60
+#930 = 90
 #6pm = 600
 
 class Employee
-  attr_accessor :times_cant_work
-  attr_reader :name
+  attr_reader :name, :times_cant_work
 
   def initialize name
     @name = name
+    @times_cant_work = []
   end
+
+  def times_cant_work
+    puts
+    puts "Please enter times #{@name} can't work (ex. 1000-1130) or enter -1 if finished entering times."
+    times = gets.chomp
+    while times != '-1'
+      @times_cant_work << convert_time(times)
+      puts
+      puts "Other times #{@name} can't work (ex. 1000-1130)? Enter -1 if finished entering times."
+      times = gets.chomp
+    end
+  end
+
+  private
+
+    def convert_time times
+      times = times.split("-")
+      hours = times.map do |time|
+        time = time.to_i
+        if time >= 800
+          ((time - 800)/100 * 60) + (time % 100)
+        else
+          ((time + 400)/100 * 60) + (time % 100)
+        end
+      end
+    end
 end
 
 class Schedule
+  attr_reader :employees
+
   def initialize
     @weekly_schedule = {
       Monday: [0..600],
@@ -35,37 +64,33 @@ class Schedule
     @employees = []
   end
 
-  def get_number_of_employees
-    puts "Please enter number of employees: "
-    @number_of_employees = gets.chomp #add validation
-  end
-
-  def create_employees
-    i = 1
-    while i <= @number_of_employees
-      puts "Please enter employee #{i}'s name:"
-      name = gets.chomp
-      employee = Employee.new name
-    end
-  end
-
   def create_schedule
+    get_number_of_employees
+    create_employees
   end
+
+  private
+
+    def get_number_of_employees
+      puts
+      puts "Please enter number of employees: "
+      @number_of_employees = gets.chomp.to_i #add validation
+    end
+
+    def create_employees
+      i = 1
+      while i <= @number_of_employees
+        puts
+        puts "Please enter employee #{i}'s name:"
+        name = gets.chomp
+        employee = Employee.new name
+        employee.times_cant_work
+        @employees << employee
+        i = i + 1
+      end
+    end
 end
 
-# ask number_of_employees
-employees = []
-
-# create employee up to number
-i = 1
-while i <= number_of_employees
-  puts "What is the name of employee #{i}?"
-  name = gets.chomp
-  employee = Employee.new name
-  puts "What times can #{employee.name} not work? (Ex. 8-10)"
-
-  employees << employee
-end
-
-schedule = Schedule.new number_of_employees
+schedule = Schedule.new
 schedule.create_schedule
+puts schedule.employees
